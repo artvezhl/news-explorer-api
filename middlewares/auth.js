62e5b7fbdba598d2db054b2ace6cbfa8b90686fe
Artemiy Vezhlivtsev <artvezhl@gmail.com>
2020-09-30 01:06:46 +0300

    change authorization from Bearer to cookies

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -2,14 +2,7 @@ const jwt = require('jsonwebtoken');
 
 const { JWT_SECRET = 'dev-secret' } = process.env;
 module.exports = (req, res, next) => {
-  const { authorization } = req.headers;
-  if (!authorization || !authorization.startsWith('Bearer ')) {
-    return res
-      .status(401)
-      .send({ message: 'Необходима авторизация' });
-  }
-
-  const token = authorization.replace('Bearer ', '');
+  const token = req.cookies.jwt;
   let payload;
 
   try {
