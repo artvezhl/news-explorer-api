5c12da9f9c43147f8cc581972d27fca3655be4db
Artemiy Vezhlivtsev <artvezhl@gmail.com>
2021-01-20 12:55:59 +0300

    change cors options

--- app.js
+++ app.js
@@ -1,18 +1,28 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const helmet = require('helmet');
-// const cors = require('cors');
+const cors = require('cors');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const { celebrateErrorsHandler } = require('./middlewares/validations');
 require('dotenv').config();
 
-// const whitelist = [
-//   'http://localhost:8080',
-//   'http://diploma-web.tk',
-//   'https://diploma-web.tk',
-// ];
-//
+const whitelist = [
+  'http://localhost:8080',
+  'http://diploma-web.tk',
+  'https://diploma-web.tk',
+];
+
+const corsOptionsDelegate = (req, callback) => {
+  let corsOptions;
+  if (whitelist.indexOf(req.header('Origin')) !== -1) {
+    corsOptions = { origin: true }; // reflect (enable) the requested origin in the CORS response
+  } else {
+    corsOptions = { origin: false }; // disable CORS for this request
+  }
+  callback(null, corsOptions); // callback expects two parameters: error and options
+};
+
 // const corsOptions = {
 //   origin: function (origin, callback) {
 //     if (whitelist.indexOf(origin) !== -1 || !origin) {
@@ -53,7 +63,9 @@ mongoose.connect(MONGO_SERVER, {
   useUnifiedTopology: true,
 });
 
-// app.use('*', cors(corsOptions));
+app.use('*', cors(corsOptionsDelegate), (req, res) => {
+  res.json({ msg: 'This is CORS-enabled for a whitelisted domain.' });
+});
 
 // подключение логгера запросов
 app.use(requestLogger);
