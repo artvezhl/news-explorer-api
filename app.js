e989dd01db345a2c5eb48228992acb50f045a45a
Artemiy Vezhlivtsev <artvezhl@gmail.com>
2021-01-20 15:52:23 +0300

    change cors options

--- app.js
+++ app.js
@@ -15,12 +15,23 @@ const whitelist = [
 
 const corsOptions = {
   origin: (origin, callback) => {
-    if (whitelist.indexOf(origin) !== -1) {
+    if (whitelist.indexOf(origin) !== -1 || !origin) {
       callback(null, true);
     } else {
       callback(new Error('Not allowed by CORS'));
     }
   },
+  methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
+  preflightContinue: false,
+  optionsSuccessStatus: 204,
+  allowedHeaders: [
+    'Content-Type',
+    'origin',
+    'x-access-token',
+    'authorization',
+    'credentials',
+  ],
+  credentials: true,
 };
 
 const { PORT, MONGO_SERVER } = require('./config');
