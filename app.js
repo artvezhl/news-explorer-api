80565863d6c926513afa4a4c399ab7e7d0bb87d4
Artemiy Vezhlivtsev <artvezhl@gmail.com>
2021-01-20 13:02:11 +0300

    change cors options

--- app.js
+++ app.js
@@ -13,36 +13,15 @@ const whitelist = [
   'https://diploma-web.tk',
 ];
 
-const corsOptionsDelegate = (req, callback) => {
-  let corsOptions;
-  if (whitelist.indexOf(req.header('Origin')) !== -1) {
-    corsOptions = { origin: true }; // reflect (enable) the requested origin in the CORS response
-  } else {
-    corsOptions = { origin: false }; // disable CORS for this request
-  }
-  callback(null, corsOptions); // callback expects two parameters: error and options
+const corsOptions = {
+  origin: (origin, callback) => {
+    if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
 };
-
-// const corsOptions = {
-//   origin: function (origin, callback) {
-//     if (whitelist.indexOf(origin) !== -1 || !origin) {
-//       callback(null, true);
-//     } else {
-//       callback(new Error('Not allowed by CORS'));
-//     }
-//   },
-//   methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
-//   preflightContinue: false,
-//   optionsSuccessStatus: 200,
-//   allowedHeaders: [
-//     'Content-Type',
-//     'origin',
-//     'x-access-token',
-//     'authorization',
-//     'credentials',
-//   ],
-//   credentials: true,
-// };
 
 const { PORT, MONGO_SERVER } = require('./config');
 
@@ -63,9 +42,7 @@ mongoose.connect(MONGO_SERVER, {
   useUnifiedTopology: true,
 });
 
-app.use('*', cors(corsOptionsDelegate), (req, res) => {
-  res.json({ msg: 'This is CORS-enabled for a whitelisted domain.' });
-});
+app.use('*', cors(corsOptions));
 
 // подключение логгера запросов
 app.use(requestLogger);
