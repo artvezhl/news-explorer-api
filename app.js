e6df1957894632c4145592bb01f5ca0ceba2bb93
Artemiy Vezhlivtsev <artvezhl@gmail.com>
2021-01-24 17:30:24 +0300

    fix card image size

--- app.js
+++ app.js
@@ -9,6 +9,10 @@ require('dotenv').config();
 
 const whitelist = [
   'http://localhost:8080',
+  'http://api.diploma-web.tk',
+  'https://api.diploma-web.tk',
+  'http://www.api.diploma-web.tk',
+  'https://www.api.diploma-web.tk',
   'https://www.diploma-web.tk',
   'http://www.diploma-web.tk',
 ];
